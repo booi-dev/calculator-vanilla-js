d3d3a946530aecc54192a47022d1767fa2864350
Booi <56810550+booimangang@users.noreply.github.com>
2022-12-01 23:39:40 +0530

    Add primary operator functions

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
+function allClear() {
+    console.log("delete all")
+}
+
+function clear() {
+    console.log("delete one digit")
+}
+
+function division(a, b) {
+    return a / b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+function subtraction(a, b) {
+    return a - b;
+}
+
+function addition(a, b) {
+    return a + b;
+}
+
+function negation(data) {
+    return data * -1;
+}
+
+const display = document.querySelector(".display")
+
+function printOnDisplay(content) {
+    display.innerText = content
+}
+
+
+
 const keypad = document.querySelector(".keypad")
 const key = keypad.querySelectorAll("div")
-
-// key.addEventListener("click", getDataKey)
 
 key.forEach(element => {
     element.addEventListener("click", getDataKey)
 })
 
 function getDataKey(e) {
-    console.log(e.target.dataset.key)
+    let dataSet = e.target.dataset.key
+    console.log(dataSet)
+    printOnDisplay(dataSet)
 }
 
