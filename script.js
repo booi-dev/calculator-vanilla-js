0d82c15a00115d15afcd556ac40dd58e4d6ab5e9
Booi <56810550+booimangang@users.noreply.github.com>
2022-12-02 00:23:08 +0530

    Add update display method

--- script.js
+++ script.js
@@ -26,24 +26,70 @@ function negation(data) {
     return data * -1;
 }
 
+// Selectors
+
 const display = document.querySelector(".display")
 
 function printOnDisplay(content) {
     display.innerText = content
 }
 
-
-
 const keypad = document.querySelector(".keypad")
 const key = keypad.querySelectorAll("div")
 
+
+// Click and Display function
+
+let firstNum = "0";
+let secondNum = "0";
+
 key.forEach(element => {
-    element.addEventListener("click", getDataKey)
+    element.addEventListener("click", updateDisplay)
 })
 
-function getDataKey(e) {
-    let dataSet = e.target.dataset.key
-    console.log(dataSet)
-    printOnDisplay(dataSet)
+
+function updateDisplay(e) {
+    const data = getDataSet(e);
+    console.log("data num", data.num)
+    if (data.num) {
+        const updatedFirstNum = updateFirstNum(data.num)
+        printOnDisplay(updatedFirstNum)
+    }
 }
 
+function getDataSet(e) {
+    let dataSet = e.target.dataset
+    return dataSet;
+}
+
+function updateFirstNum(params) {
+    console.log("update first number", firstNum, params)
+
+    if (firstNum == 0) {
+        return firstNum = params;
+    } else return firstNum += params;
+}
+
+function updateSecondNum(params) {
+    secondNum += params;
+}
+
+
+function operate(a, b, operator) {
+    let result;
+    if (operator === "division") {
+        division(a, b)
+    } else if (operator === "multiply") {
+        multiplication(a, b)
+    } else if (operator === "subtraction") {
+        subtraction(a, b)
+    } else if (operator === "addition") {
+        addition(a, b)
+    }
+}
+
+
+window.onload = () => {
+    console.log("page is fully loaded");
+    printOnDisplay(firstNum)
+};
