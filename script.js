467995f20ca7ad8baee89a6bacdad2a7698db37a
Booi <56810550+booimangang@users.noreply.github.com>
2022-12-02 00:41:10 +0530

    Add clearAll function

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-function allClear() {
-    console.log("delete all")
-}
+// VALUE DECLARATION
 
-function clear() {
-    console.log("delete one digit")
+let firstNum = "0";
+let secondNum = "0";
+
+// SELECTORS
+
+const display = document.querySelector(".display")
+
+const keypad = document.querySelector(".keypad")
+const numKey = keypad.querySelectorAll(".num-key")
+
+const ACBtn = document.querySelector(`.secondary-operators[data-key ="allClear"]`)
+
+
+// FUNCTIONS
+
+function printOnDisplay(content) {
+    display.innerText = content
 }
 
 function division(a, b) {
@@ -26,31 +39,22 @@ function negation(data) {
     return data * -1;
 }
 
-// Selectors
+// Secondary Functions
 
-const display = document.querySelector(".display")
-
-function printOnDisplay(content) {
-    display.innerText = content
+function allClear() {
+    firstNum = "0"
+    printOnDisplay(firstNum)
+    console.log("delete all")
 }
 
-const keypad = document.querySelector(".keypad")
-const key = keypad.querySelectorAll("div")
-
-
-// Click and Display function
-
-let firstNum = "0";
-let secondNum = "0";
-
-key.forEach(element => {
-    element.addEventListener("click", updateDisplay)
-})
+function clear() {
+    console.log("delete one digit")
+}
 
 
 function updateDisplay(e) {
     const data = getDataSet(e);
-    console.log("data num", data.num)
+    // console.log("data num", data.num)
     if (data.num) {
         const updatedFirstNum = updateFirstNum(data.num)
         printOnDisplay(updatedFirstNum)
@@ -63,8 +67,7 @@ function getDataSet(e) {
 }
 
 function updateFirstNum(params) {
-    console.log("update first number", firstNum, params)
-
+    // console.log("update first number", firstNum, params)
     if (firstNum == 0) {
         return firstNum = params;
     } else return firstNum += params;
@@ -73,7 +76,6 @@ function updateFirstNum(params) {
 function updateSecondNum(params) {
     secondNum += params;
 }
-
 
 function operate(a, b, operator) {
     let result;
@@ -88,6 +90,17 @@ function operate(a, b, operator) {
     }
 }
 
+
+// EVENT LISTERNERS
+
+numKey.forEach(element => {
+    element.addEventListener("click", updateDisplay)
+})
+
+ACBtn.addEventListener("click", () => {
+    allClear()
+    console.log("Delete")
+})
 
 window.onload = () => {
     console.log("page is fully loaded");
