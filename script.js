6f0bfc454f9b2819376049255490afd7f57794b1
Booi <56810550+booimangang@users.noreply.github.com>
2022-12-02 13:30:37 +0530

    Add functionality to delete from both displays

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 let firstNum = "0";
 let secondNum = "0";
 let operator = "none"
+let result = 0;
 
 // SELECTORS
 
@@ -58,12 +59,16 @@ function updateDisplay(content) {
         display.classList.remove("reduceDisplayFontx1", "reduceDisplayFontx2")
     } else display.classList.remove("reduceDisplayFontx1", "reduceDisplayFontx2", "reduceDisplayFontx3")
 
-    display.innerText = content
+    display.innerText = content;
+    console.log("display one is updated", content);
 }
 
 function updateDisplay2(content) {
     display2.innerText = content
+    console.log("display two is updated", content);
 }
+
+// updateDisplay2(secondNum)
 
 operatorDisplay.innerText = "NA";
 
@@ -75,29 +80,59 @@ function updateOperatorDisplay(operator) {
     } else if (operator === "subtraction") {
         operatorDisplay.innerText = "-"
     } else if (operator === "addition") {
-        operatorDisplay.innerText = "×"
+        operatorDisplay.innerText = "x"
+    } else if (operator === "none") {
+        operatorDisplay.innerText = "NA"
     }
 }
 
+// CLEAR or DELETE FUNCTIONS
 
 function allClear() {
-    firstNum = "0"
+    firstNum = "0";
+    secondNum = "0";
+    operator = "none"
     updateDisplay(firstNum)
-    // console.log("delete all")
+    updateDisplay2(secondNum)
+    updateOperatorDisplay(operator);
 }
+
+// Bring back the firstNum to display1
+function clearDisplay2() {
+    operator = "none"
+    updateOperatorDisplay(operator)
+    updateDisplay2(secondNum)
+    updateDisplay(firstNum)
+    console.log("2nd num is zero")
+}
+
 
 function clear() {
-    if (firstNum == "0") return;
+    if (firstNum == "0") return
 
-    if (firstNum.length == 1) {
-        firstNum = "0";
-    } else {
+    console.log(operator)
+
+    function clearFirtNum() {
+        if (firstNum.length == 1) { firstNum = "0"; updateDisplay(firstNum); return; }
         const lastDigitDeletedNum = firstNum.slice(0, -1);
         firstNum = lastDigitDeletedNum;
+        updateDisplay(firstNum);
     }
-    updateDisplay(firstNum);
-}
 
+    function clearSecondNum() {
+        if (secondNum.length == 1) { secondNum = "0"; updateDisplay(secondNum); return }
+        const lastDigitDeletedNum = secondNum.slice(0, -1);
+        secondNum = lastDigitDeletedNum;
+        updateDisplay(secondNum);
+    }
+
+    if (operator == "none") clearFirtNum();
+    else if (operator != "none") {
+        // console.log(firstNum, secondNum)
+        if (secondNum === "0") clearDisplay2()
+        else clearSecondNum()
+    }
+}
 
 // UPDATE NUMBER FUNCTIONS
 
@@ -109,7 +144,7 @@ function updateNumber(e) {
 
     if (operator != "none") {
         updatedSecondNum = updateSecondNum(e.target.dataset.num)
-        updateDisplay2(updatedSecondNum)
+        updateDisplay(updatedSecondNum)
 
     } else {
         updatedFirstNum = updateFirstNum(e.target.dataset.num)
@@ -130,25 +165,25 @@ function updateSecondNum(params) {
 }
 
 function updateOperator(e) {
+    if (firstNum == 0) return
     operator = e.target.dataset.key;
+    updateDisplay(secondNum);
+    updateDisplay2(firstNum);
     updateOperatorDisplay(operator)
-    console.log("operator updated: ", e.target.dataset.key)
 }
 
 
 function operate(a, b, operator) {
-    let result;
     if (operator === "division") {
-        division(a, b)
+        result = division(a, b)
     } else if (operator === "multiply") {
-        multiplication(a, b)
+        result = multiplication(a, b)
     } else if (operator === "subtraction") {
-        subtraction(a, b)
+        result = subtraction(a, b)
     } else if (operator === "addition") {
-        addition(a, b)
+        result = addition(a, b)
     }
 }
-
 
 // EVENT LISTERNERS
 
@@ -169,9 +204,9 @@ CBtn.addEventListener("click", () => {
 })
 
 equalsBtn.addEventListener("click", () => {
-    console.log("equal btn press")
+    operate(firstNum, secondNum, operator)
+    console.log(result)
 })
-
 
 // APP INITIALIZATION
 
@@ -179,12 +214,4 @@ window.onload = () => {
     console.log("page is fully loaded");
     updateDisplay(firstNum)
 };
-
-
-
-// testing usages
-
-function cosoleLogging(e) {
-    console.log(e.target.dataset.key)
-}
 
